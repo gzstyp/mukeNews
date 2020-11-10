0a5ca90fed12e1da45cf4aa453c1c1b3852ad4b0
gzstyp <444141300@qq.com>
2020-11-10 16:05:06 +0800

    属性计算的取值示例

--- App.vue
+++ App.vue
@@ -1,13 +1,22 @@
 <script>
 	export default {
 		onLaunch: function() {
-			console.log('App Launch')
+			console.log('全局仅启动一次');
+      //todo 正常情况下是用 uni.login({});登录获取user_id的!!!
+      this.$api.get_user({user_id : '5fa4a4c10a5b540001020389'}).then(result =>{
+        const {code,data} = result;
+        if(200 === code){
+          this.$store.dispatch('set_userinfo',data);
+        }
+      }).catch(err =>{
+        console.info(err);
+      });
 		},
 		onShow: function() {
-			console.log('App Show')
+			console.log('App Show');
 		},
 		onHide: function() {
-			console.log('App Hide')
+			console.log('App Hide');
 		}
 	}
 </script>
