4a2780178d3aa2a1fc3f9cc2cdce99826519206d
gzstyp <444141300@qq.com>
2020-11-10 12:10:43 +0800

    经典的css布局-图解03-我的页面

--- pages/tabbar/my/my.vue
+++ pages/tabbar/my/my.vue
@@ -1,6 +1,28 @@
 <template>
 	<view>
-		我的
+    <view class="my-header">
+      <view class="my-header-logo">
+        <view class="my-header_logo_box">
+          <image src="/static/logo.png" mode="aspectFill"></image>
+        </view>
+        <text class="my-header_name">引路者</text>
+      </view>
+      <!-- 关注-->
+      <view class="my-header-info">
+        <view class="my-header-info_box">
+          <text class="my-header-info-title">被关注</text>
+          <text>0</text>
+        </view>
+        <view class="my-header-info_box">
+          <text class="my-header-info-title">粉丝</text>
+          <text>0</text>
+        </view>
+        <view class="my-header-info_box">
+          <text class="my-header-info-title">积分</text>
+          <text>0</text>
+        </view>
+      </view>
+    </view>
 	</view>
 </template>
 
@@ -8,15 +30,63 @@
 	export default {
 		data() {
 			return {
-				
+
 			}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
-
+  page{
+    background-color: #f5f5f5;
+  }
+  .my-header{
+    padding-bottom: 15px;
+    //头像+作者名字的样式
+    .my-header-logo{
+      display: flex;
+      flex-direction: column;/* 垂直排列 */
+      align-items: center;
+      padding-top: 30px;
+      //头像,图片一般要给一个容器包裹,给定宽高度，防止被挤压然后image宽高度100%
+      .my-header_logo_box{
+        height: 60px;
+        width: 60px;
+        border-radius: 50%;
+        overflow: hidden;
+        image{
+          height: 100%;
+          width: 100%;
+        }
+      }
+      .my-header_name{
+        margin-top: 12px;
+        font-size: 16px;
+        font-weight: bold;
+      }
+    }
+    .my-header-info{
+      margin-top: 14px;
+      //让3个文字和数字水平排列
+      display: flex;
+      //让文字和数字上下垂直排列
+      .my-header-info_box{
+        display: flex;
+        flex-direction: column;/* 上下垂直排列 */
+        justify-content: center;/* 左右居中 */
+        align-items: center;/* 上下居中 */
+        width: 100%;
+        font-size: 12px;
+        color: #999;
+        //让文字变粗且换个颜色
+        .my-header-info-title{
+          font-size: 14px;
+          color: #333;
+        }
+      }
+    }
+  }
 </style>
