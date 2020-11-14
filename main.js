87987f1cb847fafa8f97846dd20e8418a6011aef
gzstyp <444141300@qq.com>
2020-11-14 10:00:04 +0800

    删除了组件dialog

--- main.js
+++ main.js
@@ -4,15 +4,11 @@ import exports from './common/api/exports.js';
 //vuex
 import store from './store/index.js';
 
-import dialog from '@/components/dialog/index.js';//可行
-//用法：this.$dialog.confirm('你确定要删除0吗?',function(){});
-Vue.prototype.$dialog = dialog;
-
 Vue.config.productionTip = false;
 Vue.prototype.$api = exports;
 
 
-App.mpType = 'app'
+App.mpType = 'app';
 
 const app = new Vue({
   store,
