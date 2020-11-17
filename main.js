5ecfdd8ae0b9194ee26303ea5a60d9ccc2a91632
gzstyp <444141300@qq.com>
2020-11-17 23:49:13 +0800

    完成app和微信小程序的openId的登录

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
 import Vue from 'vue';
 import App from './App';
 import exports from './common/api/exports.js';
-//vuex
+// vuex
 import store from './store/index.js';
 
 Vue.config.productionTip = false;
 Vue.prototype.$api = exports;
-
+// 检查是否已登录，参数 backpage : 登录后返回的页面; backtype : 打开页面的类型[1 : redirectTo 2 : switchTab];
+// 用法 const check = this.checkLogin('/pages/tabbar/follow/follow',2);if(!check)return;
 Vue.prototype.checkLogin = function(backpage, backtype){
-	var SUID  = uni.getStorageSync('SUID');
-	var SRAND = uni.getStorageSync('SRAND');
-	var SNAME = uni.getStorageSync('SNAME');
-	var SFACE = uni.getStorageSync('SFACE');
-	if(SUID == '' || SRAND == '' || SFACE == ''){
+	var userInfo  = uni.getStorageSync('USERINFO');
+	if(userInfo === null || userInfo === ''){
 		uni.redirectTo({url:'../login/login?backpage='+backpage+'&backtype='+backtype});
 		return false;
 	}
-	return [SUID, SRAND, SNAME, SFACE];
+	return userInfo;
 };
 //扩展方法,其用法:this.dialog('提交成功,感谢参与');
 Vue.prototype.dialog = function(msg){
--- pages/tabbar/my/wechatLogin.vue
+++ pages/tabbar/my/wechatLogin.vue
@@ -1,8 +1,15 @@
 <template>
 	<view>
     <titlebar backIcon title="微信登录"></titlebar>
+    <!-- 仅在微信小程序执行 -->
+    <!-- #ifdef MP-WEIXIN -->
     <!-- <button @tap="loginWeChat()">获取授权</button> -->
     <button type="primary" open-type='getUserInfo' @tap="loginWeChat()">获取授权</button>
+    <!-- #endif -->
+    <!-- 仅在微信小程序执行 -->
+    <!-- #ifdef MP-WEIXIN -->
+    <button type="primary" open-type="getUserInfo" @getuserinfo="getUserInfos">使用微信登录</button>
+    <!-- #endif -->
     <view>
       <text selectable>openId：{{openId}}</text>
     </view>
@@ -26,6 +33,65 @@
         userInfo : {}
 			};
 		},
+    onLoad(params) {
+      //仅在app端执行-开始
+      //#ifdef APP-PLUS
+      uni.login({
+        complete: (infoRes) => {
+          console.info('--------------------authResult--------------------');
+          console.info(infoRes);
+          console.info(infoRes.errMsg);
+          console.info(infoRes.authResult);
+          console.info('openid:'+infoRes.authResult.openid);
+          console.info('unionId:'+infoRes.authResult.unionid);
+          console.info('--------------------authResult--------------------');
+          const loginMsg = infoRes.errMsg;
+          if(loginMsg === 'login:ok'){
+            uni.getUserInfo({
+              complete : (res) => {
+                console.info('--------------------userInfo--------------------');
+                console.info(res);
+                const infoMsg = res.errMsg;
+                if(infoMsg === 'getUserInfo:ok'){
+                  console.info(res.userInfo);
+                  console.info('openId:'+res.userInfo.openId);
+                  console.info('unionId:'+res.userInfo.unionId);
+                }else{
+                  this.dialog('微信登录授权失败');
+                }
+                console.info('--------------------userInfo--------------------');
+              }
+            });
+          }
+        }
+      });
+      //#endif
+      //仅在app端执行-结束
+
+      // 仅在微信小程序执行-开始
+      // #ifdef MP-WEIXIN
+      // 调用 微信 login 获取 code
+      uni.login({
+          success: (res) => { //仅拿到code数据
+            console.info(res);
+              //通过code和个人基本信息获取openId或unionId,http://www.hcoder.net/tutorials/info/id/1374
+              /* uni.request({
+                  url:_self.apiServer+'member&m=codeToSession&code='+res.code,
+                  success: (sessions) => { //此处返回openId和session_key
+                      // sessions.date 数据格式
+                      // expires_in:7200
+                      // openid:"oS6of0V0rdp9nY_BuvCnQUasOHYc"
+                      // session_key:"87sE2oDD8lc+aDJj0tB6+g=="
+                      // 获取 unionId
+                      session_key = sessions.data.session_key;
+                      openid      = sessions.data.openid;
+                  },
+              }); */
+          }
+      });
+      // #endif
+      // 仅在微信小程序执行-接收
+    },
     methods:{
       loginWeChat : function(){
         const _this = this;
@@ -75,7 +141,50 @@
         		console.info(err);
         	}
         });
-      }
+      },
+      // 仅在微信小程序执行-开始
+      // #ifdef MP-WEIXIN
+      getUserInfos : (info) => {
+        console.info(info);
+        info = info.mp.detail.userInfo;
+        //userInfo {"nickName":"深海","gender":1,...avatarUrl":"https://7tdPvkPaJlkaLFFbLAffGVApluZdanLkDVplNlAhq1EJA/132"}
+        // 与服务器交互将数据提交到服务端数据库
+        /* uni.request({
+            url: _self.apiServer+'member&m=login',
+            method: 'POST',
+            header: {'content-type' : "application/x-www-form-urlencoded"},
+            data: {
+                openid : openid,
+                name   : info.nickName,
+                face   : info.avatarUrl,
+            },
+            success: res => {
+                console.log(res);
+                res = res.data;
+                // 登录成功 记录会员信息到本地
+                if(res.status == 'ok'){
+                    uni.showToast({title:"登录成功"});
+                    uni.setStorageSync('SUID' , res.data.u_id + '');
+                    uni.setStorageSync('SRAND', res.data.u_random + '');
+                    uni.setStorageSync('SNAME', res.data.u_name + '');
+                    uni.setStorageSync('SFACE', res.data.u_face + '');
+                    // 跳转
+                    if(pageOptions.backtype == 1){
+                        uni.redirectTo({url:pageOptions.backpage});
+                    }else{
+                        uni.switchTab({url:pageOptions.backpage});
+                    }
+                }else{
+                    uni.showToast({title:res.data});
+                }
+            },
+            fail: (e) => {
+                console.log(JSON.stringify(e));
+            }
+        }); */
+      },
+      // #endif
+      //仅在微信小程序执行-结束
     }
 	}
 </script>
